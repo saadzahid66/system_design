88129c11fbe086c88bdb7c5dc6ecb6d3fc6ff507
Hippolyte Foulongne <hippolyte.foulongne@tuni.fi>
2022-11-30 11:07:10 +0200

    Add copy file script

---
+++ TUNI.COMP.CE.400/product/Exercise_Returns/1.0/Exercise07/copy.sh
@@ -0,0 +1,6 @@
+scp ~/shared_p/18/TUNI.fi/ip.swp.stack/CameraDriver/1.1_student/bin/camera_driver.ko root@192.168.0.2:
+scp ~/shared_p/18/TUNI.fi/ip.swp.stack/KvazaarDriver/1.1/bin/ip_acc_driver.ko root@192.168.0.2:
+scp ~/shared_p/18/TUNI.fi/ip.sw/CameraUser/1.1/cameraUser root@192.168.0.2:
+scp ~/shared_p/18/TUNI.fi/product/Kvazaar.Veek-System/1.0/sw_Veek-System/Kvazaar_0/Kvazaar_0 root@192.168.0.2:
+scp  ~/shared_p/18/TUNI.COMP.CE.400/product/Exercise_Returns/1.0/Exercise07/encode_run.sh root@192.168.0.2:
+
